ea570939d4d367f752d0f42b234a55e655745bdb
Viraj76 <virajg.7680@gmail.com>
2023-05-04 23:13:15 +0530

    model for bounding box

--- app/src/main/java/com/example/facedetection/BoxWithText.kt
+++ app/src/main/java/com/example/facedetection/BoxWithText.kt
@@ -3,20 +3,18 @@ package com.example.facedetection
 import android.graphics.Rect
 import android.graphics.RectF
 
-data class BoxWithText(
-    var text: String? = null,
-    var rect: Rect? = null
-) {
+class BoxWithText {
+    var text: String
+    var rect: Rect
 
+    constructor(text: String, rect: Rect) {
+        this.text = text
+        this.rect = rect
+    }
 
-//    fun BoxWithText(text: String?, rect: Rect?) {
-//        this.text = text
-//        this.rect = rect
-//    }
-//
-//    fun BoxWithText(displayName: String?, boundingBox: RectF) {
-//        text = displayName
-//        rect = Rect()
-//        boundingBox.round(rect!!)
-//    }
+    constructor(displayName: String, boundingBox: RectF) {
+        text = displayName
+        rect = Rect()
+        boundingBox.round(rect)
+    }
 }
